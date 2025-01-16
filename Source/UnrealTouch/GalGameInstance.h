ecc06d295d65bfb53a21f51e8b1c938416214b4d
Yin <1324236706@qq.com>
2025-01-16 15:23:48 +0800

    01-16
    第一次存档

---
+++ Source/UnrealTouch/GalGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GalGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTOUCH_API UGalGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
