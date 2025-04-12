7e6f5f2d0edbd9c135f7ef41dfbac9021f679a40
treasureGrove <1324236706@qq.com>
2025-04-12 14:07:19 +0800

    大改

---
+++ Source/GalGameKit/GalGameKit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GalGameKit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GalGameKit, "GalGameKit" );
--- Source/UnrealTouch/UnrealTouch.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealTouch.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealTouch, "UnrealTouch" );
